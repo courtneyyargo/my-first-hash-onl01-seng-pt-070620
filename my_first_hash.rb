8a6864e59f790fe81bda48125827c80b766c66d0
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-16 01:47:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,11 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-   
+   shipping_manifest = {"muskets" => 2}
+   shipping_manifest = {"gun powder" => 4}
+   shipping_manifest = {""
+  "name" => "Corinna",
+  "age" => 36
 
 
   # add 4 gun powder to the shipping_manifest hash below
